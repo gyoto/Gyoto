ab712dc4d004fb9cac80f971adb6cf7c43288ea2
Frederic Vincent <frederic.vincent@obspm.fr>
2012-01-19 16:53:46 +0100

    + Added class DynamicalDisk that deals with evolving disk

---
+++ include/GyotoDynamicalDisk.h
@@ -0,0 +1,123 @@
+/**
+ * \file GyotoDynamicalDisk.h
+ * \brief A geometrically thin, optically thick disk, evolving dynamically
+ *
+ *  The disk is described by a set of FITS files for a set of different times
+ */
+
+/*
+    Copyright 2011 Frederic Vincent, Thibaut Paumard
+
+    This file is part of Gyoto.
+
+    Gyoto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gyoto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gyoto.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GyotoDynamicalDisk_H_ 
+#define __GyotoDynamicalDisk_H_ 
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+
+namespace Gyoto{
+  namespace Astrobj { class DynamicalDisk; }
+}
+
+//#include <GyotoMetric.h>
+#include <GyotoPatternDiskBB.h>
+
+/**
+ * \class Gyoto::Astrobj::DynamicalDisk
+ * \brief Geometrically thin disk read from a set of FITS files
+ * 
+ *   This class describes a PatternDiskBB that evolves dynamically. 
+ *   It is described by a set of FITS files.
+ *
+ */
+class Gyoto::Astrobj::DynamicalDisk : public Astrobj::PatternDiskBB {
+  friend class Gyoto::SmartPointer<Gyoto::Astrobj::DynamicalDisk>;
+ private:
+  char* dirname_; ///< FITS files directory
+  double tinit_; ///< Time of the first FITS file
+  double dt_; ///< Time increment between two FITS (assumed constant)
+  int nb_times_; ///< Number of times
+
+  /**
+   * An array of dimensionality double[nr_][nphi_][nnu_]. In FITS
+   * format, the first dimension is nu, the second phi, and the third
+   * r.
+   */
+  double ** emission_array_;
+
+  double ** opacity_array_; ///< same dimenstions as emission, or NULL
+
+  /**
+   * An array of dimensionality double[nr_][nphi_][2]. In FITS format,
+   * the second dimension is phi, and the third r. The first plane in
+   * the first FITS dimention is dphi/dt, the second dr/dt.
+   */
+  double ** velocity_array_; ///< velocity(, r, phi)
+
+  /**
+   * In case of adaptive grid.
+   */
+  double ** radius_array_; ///< radius vector
+
+  double * dnu_array_;
+  double * nu0_array_;
+  size_t * nnu_array_;
+
+  double * dphi_array_;
+  size_t * nphi_array_;
+
+  double * dr_array_;
+  size_t * nr_array_;
+  //  double r0_; // this is rin_
+
+  // Constructors - Destructor
+  // -------------------------
+ public:
+  DynamicalDisk(); ///< Standard constructor
+  
+  DynamicalDisk(const DynamicalDisk& ) ;///< Copy constructor
+  virtual DynamicalDisk* clone () const; ///< Cloner
+  
+  virtual ~DynamicalDisk() ;                        ///< Destructor
+  
+  // Accessors
+  // ---------
+ public:
+
+  virtual int setParameter(std::string name, std::string content);
+
+  virtual double emission(double nu_em, double dsem,
+			  double c_ph[8], double c_obj[8]) const;
+
+  void getVelocity(double const pos[4], double vel[4]);
+  double const * const getVelocity() const;
+  
+ protected:
+
+  void copyQuantities(int iq) ;
+
+ public:
+#ifdef GYOTO_USE_XERCES
+  virtual void fillElement(FactoryMessenger *fmp) const ;
+#endif
+
+};
+
+#endif
---
+++ lib/DynamicalDisk.C
@@ -0,0 +1,218 @@
+/*
+    Copyright 2011 Frederic Vincent, Thibaut Paumard
+
+    This file is part of Gyoto.
+
+    Gyoto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gyoto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gyoto.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#define throwCfitsioError(status) \
+    { fits_get_errstatus(status, ermsg); throwError(ermsg); }
+
+#include "GyotoPhoton.h"
+#include "GyotoDynamicalDisk.h"
+#include "GyotoUtils.h"
+#include "GyotoFactoryMessenger.h"
+#include "GyotoKerrBL.h"
+#include "GyotoKerrKS.h"
+
+
+#include <fitsio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include <limits>
+#include <sstream>
+#include <dirent.h>
+
+using namespace std;
+using namespace Gyoto;
+using namespace Gyoto::Astrobj;
+
+DynamicalDisk::DynamicalDisk() :
+  PatternDiskBB(),
+  tinit_(0.), dt_(1.)
+{
+  GYOTO_DEBUG << "DynamicalDisk Construction" << endl;
+}
+
+DynamicalDisk::DynamicalDisk(const DynamicalDisk& o) :
+  PatternDiskBB(o),
+  tinit_(o.tinit_), dt_(o.dt_)
+{
+  GYOTO_DEBUG << "DynamicalDisk Copy" << endl;
+}
+DynamicalDisk* DynamicalDisk::clone() const
+{ return new DynamicalDisk(*this); }
+
+DynamicalDisk::~DynamicalDisk() {
+  GYOTO_DEBUG << "DynamicalDisk Destruction" << endl;
+  delete [] emission_array_;
+  delete [] opacity_array_;
+  delete [] velocity_array_;
+  delete [] radius_array_;
+  delete [] dnu_array_;
+  delete [] nu0_array_;
+  delete [] nnu_array_;
+  delete [] nphi_array_;
+  delete [] nr_array_;
+}
+
+double const * const DynamicalDisk::getVelocity() const { return PatternDiskBB::getVelocity(); }
+
+void DynamicalDisk::copyQuantities(int iq) {
+  double * curem = emission_array_[iq-1],
+    * curop = NULL, //temporary
+    * curvel = velocity_array_[iq-1], 
+    * curr = radius_array_[iq-1];
+
+  setEmission(curem);
+  setVelocity(curvel);
+  setRadius(curr);
+}
+
+void DynamicalDisk::getVelocity(double const pos[4], double vel[4]) {
+  double time = pos[0], tcomp=tinit_;
+  int ifits=1;
+  while(time>tcomp && ifits<nb_times_){
+    tcomp+=dt_;
+    ifits++;
+  }
+
+  //cout << "time stuff= " << time << " " << tinit_ << " " << dt_ << " " << ifits << endl;
+  
+  if (ifits==1 || ifits==nb_times_){
+    copyQuantities(ifits);
+    PatternDiskBB::getVelocity(pos,vel);
+  }else{
+    double vel1[4], vel2[4];
+    copyQuantities(ifits-1);
+    PatternDiskBB::getVelocity(pos,vel1);
+    copyQuantities(ifits);
+    PatternDiskBB::getVelocity(pos,vel2);
+    for (int ii=0;ii<4;ii++){ // 1st order interpol
+      double t1 = tinit_+(ifits-2)*dt_;
+      vel[ii]=vel1[ii]+(vel2[ii]-vel1[ii])/dt_*(time-t1);
+    }
+  }
+}
+
+double DynamicalDisk::emission(double nu, double dsem,
+			       double *,
+			       double co[8]) const {
+  GYOTO_DEBUG << endl;
+  double time = co[0], tcomp=tinit_;
+  int ifits=1;
+  while(time>tcomp && ifits<nb_times_){
+    tcomp+=dt_;
+    ifits++;
+  }
+
+  double* fake;
+  if (ifits==1 || ifits==nb_times_){
+    const_cast<DynamicalDisk*>(this)->copyQuantities(ifits); //awful trick to avoid problems with constness of function emission -> to improve
+    return PatternDiskBB::emission(nu,dsem,fake,co);
+  }else{
+    double I1, I2;
+    const_cast<DynamicalDisk*>(this)->copyQuantities(ifits-1);
+    I1=PatternDiskBB::emission(nu,dsem,fake,co);
+    const_cast<DynamicalDisk*>(this)->copyQuantities(ifits);
+    I2=PatternDiskBB::emission(nu,dsem,fake,co);
+    double t1 = tinit_+(ifits-2)*dt_;
+    return I1+(I2-I1)/dt_*(time-t1);
+  }
+ 
+  return 0.;
+}
+
+int DynamicalDisk::setParameter(std::string name, std::string content) {
+  if (name == "File") {
+    dirname_ = new char[strlen(content.c_str())+1];
+    strcpy(dirname_,content.c_str());
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dirname_)) == NULL) {
+      throwError("In DynamicalDisk.C constructor : bad dirname_");
+    }
+    
+    nb_times_=0;
+    while ((dirp = readdir(dp)) != NULL) {
+      nb_times_++;
+    }
+    nb_times_-=2; //for directories . and .. 
+    
+    /*
+      NB: ***Caution***, here it is assumed that dirname_ 
+      contains ONLY the FITS files, nothing else.
+    */
+    closedir(dp);
+    
+    GYOTO_DEBUG << "FITS directory, number of FITS files= " << 
+      dirname_ << " " << nb_times_ << endl;
+    
+    if (nb_times_<1) 
+      throwError("In DynamicalDisk.C: bad nb_times_ value");
+    
+    emission_array_ = new double*[nb_times_] ;
+    opacity_array_ = new double*[nb_times_] ;
+    velocity_array_ = new double*[nb_times_] ;
+    radius_array_ = new double*[nb_times_] ;
+    dnu_array_ = new double[nb_times_];
+    nu0_array_ = new double[nb_times_];
+    nnu_array_ = new size_t[nb_times_];
+    nphi_array_ = new size_t[nb_times_];
+    nr_array_ = new size_t[nb_times_];
+    
+    for (int i=1; i<=nb_times_; i++) {
+      ostringstream stream_name ;
+      stream_name << dirname_ << "pseudoN2D" 
+		  << setw(4) << setfill('0') 
+		  << i << ".fits.gz" ;
+      
+      string filename = stream_name.str();
+      GYOTO_DEBUG << "Reading FITS file: " << filename << endl ;
+      fitsRead(filename);
+      size_t naxes[3];
+      getIntensityNaxes(naxes);
+      size_t nnu,nphi,nr,
+	nel = (nnu=naxes[0])*(nphi=naxes[1])*(nr=naxes[2]);
+      emission_array_[i-1]=const_cast<double*>(getIntensity());
+      opacity_array_[i-1]=const_cast<double*>(getOpacity());
+      velocity_array_[i-1]=const_cast<double*>(getVelocity());
+      radius_array_[i-1]=const_cast<double*>(getGridRadius());
+      dnu_array_[i-1]=dnu();
+      nu0_array_[i-1]=nu0();
+      nnu_array_[i-1]=nnu;
+      nphi_array_[i-1]=nphi;
+      nr_array_[i-1]=nr;
+    }
+
+  }
+  else if (name=="tinit") tinit_=atof(content.c_str());
+  else if (name=="dt") dt_=atof(content.c_str());
+  else return PatternDiskBB::setParameter(name, content);
+  return 0;
+}
+
+#ifdef GYOTO_USE_XERCES
+void DynamicalDisk::fillElement(FactoryMessenger *fmp) const {
+  if (tinit_) fmp->setParameter("tinit", tinit_);
+  if (dt_) fmp->setParameter("dt", dt_);
+  PatternDiskBB::fillElement(fmp);
+}
+
+#endif
