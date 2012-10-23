0d1faa78acbda296c0a66e8960f5bf54db6991be
Thibaut Paumard <paumard@users.sourceforge.net>
2012-10-23 14:37:11 +0200

    Document include/GyotoConverters.h

--- include/GyotoConverters.h
+++ include/GyotoConverters.h
@@ -1,8 +1,50 @@
 /**
  * \file GyotoConverters.h
- * \brief GYOTO utilities
+ * \brief GYOTO converters
  *
- *  Various utilities
+ *  As a (recommended) option, Gyoto can use the libudunits2 library
+ *  by UCAR/Unidata
+ *  (http://www.unidata.ucar.edu/software/udunits/udunits-2/udunits2.html)
+ *  to perform conversions to and from various units. Since udunits
+ *  parses units, the following are equivalent for instance:
+ *  "mJy.sr-1", "mJy/sr", "1e-3Jy/sr".
+ *
+ *  In addition to the stock units known by udunits2, Gyoto registers
+ *  the following (which may be used in any context): Jansky (symbol
+ *  Jy), symbol "pc" for parsec, "sunradius", "sunmass".
+ *  
+ *  Other units are context-sensitive: "geometrical" allows converting
+ *  between geometrical units and other legnth units, but only when a
+ *  Metric is defined, and may not be used (yet) in compound
+ *  units. Likewise, "geometrical_time" can be used as duration unit
+ *  whenever a Metric is defined, but not in a compound unit.
+ *
+ *  When a Screen is defined, "pix" can be used as an angle unit (you
+ *  need to call Screen::mapPixUnit() and Screen::unmapScreenUnit(),
+ *  which is done automatically in certain contexts).
+ *
+ *  Units can often be specified using the "unit" XML attribute:
+ * \code
+ * <Meric kind="KerrBL">
+ *    <Mass unit="sunmass">
+ *       4e6
+ *    </Mass>
+ * </Metric>
+ * \endcode
+ *
+ * Units for output quantities are specified after the name of the
+ * quantity, in squared brackets, e.g.:
+ * \code
+ *   <Quantities>
+ *     Spectrum[erg.s-1.cm-2.sr-1.Hz-1]
+ *   </Quantities>
+ * \endcode
+ * or
+ * \code
+ *   <Quantities>
+ *     Spectrum[mJy/pix2]
+ *   </Quantities>
+ * \endcode
  */
 
 /*
@@ -42,27 +84,224 @@ namespace Gyoto {
     class Generic;
   }
   namespace Units {
+    /**
+     * \namespace Gyoto::Units
+     * \brief Units-related classes and functions
+     */ 
 #ifdef HAVE_UDUNITS
+    /**
+     * \class Gyoto::Units::Unit
+     * \brief Wrapper around ut_unit from udunits
+     */
     class Unit;
+
+    /**
+     * \class Gyoto::Units::Converter
+     * \brief Wrapper around ut_converter from udunits
+     *
+     * A Gyoto::Units::Converter object is a functor and can be used
+     * to convert efficiently between the two units specified at
+     * instantiation time:
+     *
+     * \code
+     * string from_unit="erg.s-1.cm-2.sr-1.Hz-1", to_unit="mJy/microarcsec2";
+     * double data_in[1000], data_out[1000];
+     * Converter conv(from_unit, to_unit);
+     * for (size_t i=0; i<1000; ++i) data_out[i] = conv(data_in[i]);
+     * \endcode
+     *
+     */
     class Converter;
+
+    /**
+     * \brief The unit system used in all of Gyoto
+     */
     ut_system * getSystem();
 #endif
+
+    /**
+     * \brief Load and initialize all (non-context-sensitive) units
+     *
+     * If udunits is used (preprocessor macro HAVE_UDUNITS), Init()
+     * initializes the ut_system used throughout Gyoto and maps a few
+     * additional units to the unit system.
+     */
     void Init();
-    double ToMeters(double, const std::string &,
+
+    /**
+     * \brief Convert from arbitrary length unit to meters
+     *
+     * Convert value from unit represented by "unit" to meters.
+     *
+     * If gg is provided (and not NULL), use it to interpret the
+     * string "geometrical" as representing
+     * gg->Gyoto::Metric::Generic::unitLength().
+     *
+     * \param value
+     *   (double) the value to convert, expressed according to "unit"
+     * \param unit (std::string) the "unit" from which to convert,
+     *   e.g. "km", "sunradius" or "geometrical". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     * \param gg (const Gyoto::SmartPointer<Gyoto::Metric::Generic> &,
+     *   NULL if not specified) optional metric to interpret
+     *   "geometrical".
+     *
+     * \return value, expressed in meters.
+     */
+    double ToMeters(double value, const std::string &unit,
 		  const Gyoto::SmartPointer<Gyoto::Metric::Generic> &gg=NULL);
+
+    /**
+     * \brief Convert to arbitrary length unit from meters
+     *
+     * Convert value to unit represented by "unit" from meters.
+     *
+     * If gg is provided (and not NULL), use it to interpret the
+     * string "geometrical" as representing
+     * gg->Gyoto::Metric::Generic::unitLength().
+     *
+     * \param value
+     *   (double) the value to convert, expressed in meters.
+     * \param unit (std::string) the "unit" to which to convert,
+     *   e.g. "km", "sunradius" or "geometrical". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     * \param gg (const Gyoto::SmartPointer<Gyoto::Metric::Generic> &,
+     *   NULL if not specified) optional metric to interpret
+     *   "geometrical".
+     *
+     * \return value, expressed in "unit".
+     */
     double FromMeters(double, const std::string &,
 		  const Gyoto::SmartPointer<Gyoto::Metric::Generic> &gg=NULL);
+
+    /**
+     * \brief Convert from arbitrary time unit to seconds
+     *
+     * Convert value from unit represented by "unit" to seconds.
+     *
+     * If gg is provided (and not NULL), use it to interpret the
+     * string "geometrical_time" as representing
+     * gg->Gyoto::Metric::Generic::unitLength()/GYOTO_C.
+     *
+     * \param value
+     *   (double) the value to convert, expressed according to "unit"
+     * \param unit (std::string) the "unit" from which to convert,
+     *   e.g. "s", "yr" or "geometrical_time". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     * \param gg (const Gyoto::SmartPointer<Gyoto::Metric::Generic> &,
+     *   NULL if not specified) optional metric to interpret
+     *   "geometrical".
+     *
+     * \return value, expressed in seconds.
+     */
     double ToSeconds(double, const std::string &unit,
 		  const Gyoto::SmartPointer<Gyoto::Metric::Generic> &gg=NULL);
+
+    /**
+     * \brief Convert to arbitrary time unit from seconds
+     *
+     * Convert value to unit represented by "unit" from seconds.
+     *
+     * If gg is provided (and not NULL), use it to interpret the
+     * string "geometrical_time" as representing
+     * gg->Gyoto::Metric::Generic::unitLength()/GYOTO_C.
+     *
+     * \param value
+     *   (double) the value to convert, expressed in seconds.
+     * \param unit (std::string) the "unit" to which to convert,
+     *   e.g. "s", "yr" or "geometrical_time". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     * \param gg (const Gyoto::SmartPointer<Gyoto::Metric::Generic> &,
+     *   NULL if not specified) optional metric to interpret
+     *   "geometrical".
+     *
+     * \return value, expressed in "unit".
+     */
     double FromSeconds(double, const std::string &unit,
 		  const Gyoto::SmartPointer<Gyoto::Metric::Generic> &gg=NULL);
+
+    /**
+     * \brief Convert from arbitrary mass unit to kilograms
+     *
+     * Convert value from unit represented by "unit" to kilograms.
+     *
+     * \param value
+     *   (double) the value to convert, expressed according to "unit"
+     * \param unit (std::string) the "unit" from which to convert,
+     *   e.g. "g", "kg" or "sunmass". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     *
+     * \return value, expressed in kilograms.
+     */
     double ToKilograms(double, const std::string &);
+
+    /**
+     * \brief Convert to arbitrary mass unit from kilograms
+     *
+     * Convert value from unit represented by "unit" from kilograms.
+     *
+     * \param value
+     *   (double) the value to convert, expressed inkilograms.
+     * \param unit (std::string) the "unit" to which to convert,
+     *   e.g. "g", "kg" or "sunmass". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     *
+     * \return value, expressed in "unit".
+     */
     double FromKilograms(double, const std::string &);
+
+    /**
+     * \brief Convert from arbitrary length unit to geometrical units
+     *
+     * Convert value from unit represented by "unit" to geometrical units.
+     *
+     * \param value
+     *   (double) the value to convert, expressed according to "unit".
+     * \param unit (std::string) the "unit" from which to convert,
+     *   e.g. "km", "sunradius" or "geometrical". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     * \param gg (const Gyoto::SmartPointer<Gyoto::Metric::Generic> &)
+     *   metric to interpret "geometrical".
+     *
+     * \return value, expressed in geometrical units.
+     */
     double ToGeometrical(double, const std::string &,
 		  const Gyoto::SmartPointer<Gyoto::Metric::Generic> &);
+
+    /**
+     * \brief Convert to arbitrary length unit from geometrical units
+     *
+     * Convert value to unit represented by "unit" from geometrical units.
+     *
+     * \param value
+     *   (double) the value to convert, expressed in geometrical units.
+     * \param unit (std::string) the "unit" to which to convert,
+     *   e.g. "km", "sunradius" or "geometrical". More units are
+     *   supported if Gyoto was compiled with udunits support.
+     * \param gg (const Gyoto::SmartPointer<Gyoto::Metric::Generic> &)
+     *   metric to interpret "geometrical".
+     *
+     * \return value, expressed in geometrical units.
+     */
     double FromGeometrical(double, const std::string &,
 		  const Gyoto::SmartPointer<Gyoto::Metric::Generic> &);
-    bool areConvertible(const std::string &, const std::string&);
+
+    /**
+     * \brief Is it possible to convert between unit1 and unit2?
+     *
+     * e.g. areConvertible("m", "kg") == 0; areConvertible("m", "km")==1.
+     *
+     * Warning: angle units are dimensionless, therefore e.g.
+     * areConvertible("Jy", "Jy/microacsec2")==1. Numerically, "Jy" is
+     * the same as "Jy/sr2".
+     *
+     * \param unit1 (string) first unit
+     * \param unit2 (string) second unit
+     *
+     * \return bool, True if it is possible to convert between the two
+     * units, 0 otherwise.
+     */
+    bool areConvertible(const std::string &unit1, const std::string &unit2);
   }
 }
 
@@ -71,35 +310,95 @@ class Gyoto::Units::Unit : protected Gyoto::SmartPointee {
   friend class Gyoto::SmartPointer<Gyoto::Units::Unit>;
   friend class Gyoto::Units::Converter;
  private:
-  ut_unit * unit_;
-  std::string kind_;
+  ut_unit * unit_; ///< the underlying ut_unit (from udunits)
+  std::string kind_; ///< the string used to instanciate this unit
  public:
-  Unit(const std::string &);
+
+  /**
+   * \brief Build Unit described by string
+   *
+   * Throws a Gyoto::Error if anything goes wrong.
+   *
+   * \param unit string description of the unit, e.g. "mJy/sr2" or "sunmass".
+   */
+  Unit(const std::string &unit);
+
+  /**
+   * \brief Destructor
+   *
+   * Frees unit_.
+   */
   ~Unit();
+
+  /**
+   * \brief Convert to Unit
+   *
+   * \param value double to convert
+   * \param from_unit string from which to convert
+   * 
+   * \return value converted to unit_.
+   */
   double To (double val, const std::string &from_unit);
+
+  /**
+   * \brief Convert from Unit
+   *
+   * \param value double to convert
+   * \param to_unit string to which to convert
+   * 
+   * \return value converted to "to_unit".
+   */
   double From (double val, const std::string &to_unit);
+
+  /**
+   * \brief Cast to string
+   *
+   * \return kind_
+   */
   operator std::string() const ;
+
+  /**
+   * \brief Cast to ut_unit*
+   *
+   * \return unit_
+   */
   operator ut_unit*() const ;
 };
 
 class Gyoto::Units::Converter : protected Gyoto::SmartPointee {
   friend class Gyoto::SmartPointer<Gyoto::Units::Converter>;
  private:
-  Gyoto::SmartPointer<Gyoto::Units::Unit> from_;
-  Gyoto::SmartPointer<Gyoto::Units::Unit> to_;
-  cv_converter * converter_;
-  void resetConverter_();
+  Gyoto::SmartPointer<Gyoto::Units::Unit> from_; ///< From Unit
+  Gyoto::SmartPointer<Gyoto::Units::Unit> to_; ///< To Unit
+  cv_converter * converter_; ///< Underlying ut_converter object from udunits
+  void resetConverter_(); ///< Initialize converter_ from from_ and to_
  public:
   Converter(const std::string &,
 	    const std::string &);
+  ///< Construct Converter from two strings
   Converter(const Gyoto::SmartPointer<Gyoto::Units::Unit>&,
 	    const std::string&);
+  ///< Construct Converter from Unit and string
   Converter(const std::string &,
 	    const Gyoto::SmartPointer<Gyoto::Units::Unit>&);
+  ///< Construct Converter from string and Unit
   Converter(const Gyoto::SmartPointer<Gyoto::Units::Unit>&,
 	    const Gyoto::SmartPointer<Gyoto::Units::Unit>&);
+  ///< Construct Converter from two Unit
   ~Converter();
-  double operator()(double) const ;
+  ///< Destruct converter, freeing converter_
+
+  /**
+   * \brief Actually convert data
+   *
+   * The entire Gyoto::Units::Converter class is there just for this
+   * operator, which converts value from unit from_ to unit to_.
+   *
+   * \param value double expressed in unit from_
+   *
+   * \return converted value expressed in unit to_
+   */
+  double operator()(double value) const ;
 };
 
 #endif
