90d6e082dc3eb8ebc2f48f373e2b0e9699d26d8c
Thibaut Paumard <paumard@users.sourceforge.net>
2013-03-12 14:34:52 +0100

    add missing files

---
+++ include/GyotoComplexSpectrometer.h
@@ -0,0 +1,151 @@
+/**
+ * \file GyotoComplexSpectrometer.h
+ * \brief Combine astronomical objects
+ *
+ *  An astrobj made of several astrobjs
+ */
+
+/*
+    Copyright 2011 Thibaut Paumard, Frederic Vincent
+
+    This file is part of Gyoto.
+
+    Gyoto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gyoto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gyoto.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef __GyotoComplexSpectrometer_H_ 
+#define __GyotoComplexSpectrometer_H_ 
+
+#include <GyotoSpectrometer.h>
+
+namespace Gyoto{
+  namespace Spectrometer {
+    class Complex;
+  }
+}
+
+/**
+ * \class Gyoto::Spectrometer::Complex
+ * \brief Complex astronomical object
+ *
+ *  A Gyoto::Spectrometer::Generic whic contain several
+ *  Gyoto::Spectrometer::Generic instances. It is essentially a
+ *  SmartPointer<Spectrometer::Generic> array, which some methods
+ *  arround. Indeed, the operator[](size_t i) method is implemented to retrived
+ *  the i-th element.
+ *
+ * In an XML description, the
+ *  &lt;Spectrometer&gt; section must be unique, its kind is
+ *  "Complex". Each sub-astrobj then appears as a
+ *  &lt;SubSpectrometer&gt; subsection:
+\code
+  <Spectrometer kind = "Complex">
+    <SubSpectrometer kind = "ThinInfiniteDiskBL"/>
+    <SubSpectrometer kind = "Star">
+      <Radius> 2. </Radius>
+      <Velocity> 0. 0. 0.037037 </Velocity>
+      <Position> 600. 9. 1.5707999999999999741 0 </Position>
+      <Spectrum kind="PowerLaw">
+	<Exponent> 0 </Exponent>
+	<Constant> 0.001 </Constant>
+      </Spectrum>
+      <Opacity kind="PowerLaw">
+	<Exponent> 0 </Exponent>
+	<Constant> 0.01 </Constant>
+      </Opacity>
+      <OpticallyThin/>
+    </SubSpectrometer>
+  </Spectrometer>
+\endcode
+ *
+ */
+class Gyoto::Spectrometer::Complex
+: public Gyoto::Spectrometer::Generic,
+  public Gyoto::Hook::Listener
+{
+  friend class Gyoto::SmartPointer<Gyoto::Spectrometer::Complex>;
+  
+  // Data : 
+  // -----
+ protected:
+
+  /**
+   * Number of objects
+   */
+  size_t cardinal_;
+
+  Gyoto::SmartPointer<Gyoto::Spectrometer::Generic> * elements_;
+
+
+ public:
+  Complex(); ///< Default constructor.
+  Complex(const Complex& ) ; ///< Copy constructor.
+  virtual Complex* clone() const; ///< "Virtual" copy constructor
+
+  /**
+   *  Frees every SmartPointer<Spectrometer::Generic> before freed the array itself.
+   */
+  virtual ~Complex() ; ///< Destructor
+
+  // Mutators
+  // --------
+ public:
+  /**
+   * If the Spectrometer::Complex itself does not have a metric already
+   * assigned, it takes it from the new element. Else, it sets the
+   * metric in the new element to its own. This ensures that all
+   * elements use the same metric (this heuristic is not entirely
+   * fool-proof, it's safer to set the metric directly in the
+   * Spectrometer::Complex).
+   */
+  void append(Gyoto::SmartPointer<Gyoto::Spectrometer::Generic> element);
+  ///< Add element at the end of the array.
+  void remove(size_t i);
+  ///< Remove i-th element from the array.
+  size_t getCardinal() const;
+  ///< Get the number of elements in the array.
+
+  virtual void tell(Gyoto::Hook::Teller *msg); ///< Called by Teller
+
+ public:
+#ifdef GYOTO_USE_XERCES
+  virtual void fillElement(FactoryMessenger *fmp) const ;
+  virtual void setParameters(FactoryMessenger *fmp);
+#endif
+  
+  // Outputs
+  // -------
+ public:
+
+
+  /**
+   * This should work as expected:
+\code
+  SmartPointer<Spectrometer::Complex> cplx;
+  SmartPointer<Spectrometer::TypeA> objA;
+  SmartPointer<Spectrometer::TypeB> objB;
+  cplx -> append(objA);
+  cplx[0] = objB;
+\endcode
+   */
+  Gyoto::SmartPointer<Gyoto::Spectrometer::Generic> operator[](size_t i) ;
+  ///< Retrieve i-th element.
+  Gyoto::SmartPointer<Gyoto::Spectrometer::Generic> const operator[](size_t i) const;
+  ///< Retrieve a const version of the i-th element.
+  
+ protected:
+};
+
+#endif
---
+++ lib/ComplexSpectrometer.C
@@ -0,0 +1,182 @@
+/*
+    Copyright 2011 Thibaut Paumard, Frederic Vincent
+
+    This file is part of Gyoto.
+
+    Gyoto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gyoto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gyoto.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "GyotoComplexSpectrometer.h"
+#include "GyotoFactoryMessenger.h"
+#include "GyotoMetric.h"
+#include "GyotoPhoton.h"
+
+#include <cstring>
+
+using namespace std;
+using namespace Gyoto;
+using namespace Gyoto::Spectrometer;
+
+Complex::Complex() :
+  Generic("Complex"),
+  cardinal_(0),
+  elements_(NULL)
+{
+
+}
+
+Complex::Complex(const Complex& o) :
+  Generic(o),
+  cardinal_(o.cardinal_),
+  elements_(NULL)
+{
+  if (cardinal_) {
+    elements_ = new SmartPointer<Generic> [cardinal_];
+    for (size_t i=0; i< cardinal_; ++i) {
+      elements_[i] = o[i]->clone();
+    }
+  }
+}
+Complex *Complex::clone() const {return new Complex(*this); }
+
+
+Complex::~Complex()
+{
+  if (cardinal_) for (size_t i=0; i< cardinal_; ++i) elements_[i] = NULL;
+}
+
+void Complex::append(SmartPointer<Generic> e)
+{
+  GYOTO_DEBUG << "DEBUG: in Complex::append(SmartPointer<Generic> e)" << endl;
+  if (cardinal_+1 == 0) throwError("Complex::append(): OVERFLOW");
+  SmartPointer<Generic> * orig = elements_;
+  elements_ = new SmartPointer<Generic> [cardinal_+1];
+  for (size_t i=0; i< cardinal_; ++i) {
+    elements_[i] = orig[i];
+    orig[i] = NULL;
+  }
+  delete [] orig; orig = NULL;
+  elements_[cardinal_] = e;
+  ++cardinal_;
+  e->hook(this);
+  GYOTO_DEBUG << "DEBUG: out Complex::append(SmartPointer<Generic> e)" << endl;
+  tell(this);
+}
+
+SmartPointer<Generic> Complex::operator[](size_t i)
+{
+  if (i > cardinal_)
+    throwError("Complex::operator[](size_t i): no such element");
+  return elements_[i];
+}
+
+SmartPointer<Generic> const Complex::operator[](size_t i) const
+{
+  if (i > cardinal_)
+    throwError("Complex::operator[](size_t i): no such element");
+  return elements_[i];
+}
+
+void Complex::remove(size_t i) {
+  if (i >= cardinal_)
+    throwError("Complex::remove(size_t i): no such element");
+  elements_[i]->unhook(this);
+  SmartPointer<Generic> * orig = elements_;
+  if (--cardinal_) elements_ = new SmartPointer<Generic> [cardinal_];
+  else elements_ = NULL;
+  size_t k, j=0;
+  for (k=0; k<= cardinal_; ++k) {
+    if (k != i) elements_[j++] = orig[k];
+    orig[k] = NULL;
+  }
+  delete [] orig;
+  tell(this); // tell self!
+}
+
+size_t Complex::getCardinal() const {return cardinal_; }
+
+#ifdef GYOTO_USE_XERCES
+void Complex::fillElement(FactoryMessenger *fmp) const {
+  FactoryMessenger * childfmp=NULL;
+
+  for (size_t i=0; i<cardinal_; ++i) {
+    childfmp = fmp -> makeChild ( "SubSpectrometer" );
+    elements_[i] -> fillElement(childfmp);
+    delete childfmp;
+  }
+
+  Spectrometer::Generic::fillElement(fmp);
+}
+
+void Complex::setParameters(FactoryMessenger *fmp) {
+  if (debug())
+    cerr << "DEBUG: in Complex::setParameters()" << endl;
+
+  string name="", content="", unit="";
+  FactoryMessenger * child = NULL;
+
+  while (fmp->getNextParameter(&name, &content, &unit)) {
+    if (debug())
+      cerr << "DEBUG: Spectrometer::Complex::Subcontractor(): name=" << name << endl;
+    if (name=="SubSpectrometer") {
+      content = fmp -> getAttribute("kind");
+      child = fmp -> getChild();
+      append ((*Spectrometer::getSubcontractor(content))(child));
+      delete child;
+    }// else setParameter(name, content, unit);
+  }
+
+  if (debug())
+    cerr << "DEBUG: out Complex::setParameters()" << endl;
+}
+#endif
+
+void Complex::tell(Gyoto::Hook::Teller *) {
+  // This is called each time an element is added or mutated
+  // This is suboptimal, but most straightforward
+  nboundaries_=nsamples_=0;
+  for (size_t i=0; i<cardinal_; ++i) {
+    nsamples_ += elements_[i]->getNSamples();
+    nboundaries_ += elements_[i]->getNBoundaries();
+  }
+  if (boundaries_) delete [] boundaries_;
+  if (widths_)     delete [] widths_;
+  if (midpoints_)  delete [] midpoints_;
+  if (chanind_)    delete [] chanind_;
+  boundaries_ = new double [nboundaries_];
+  widths_     = new double [nsamples_];
+  midpoints_  = new double [nsamples_];
+  chanind_    = new size_t [2*nsamples_];
+  
+  size_t boffset=0, offset=0;
+  for (size_t i=0; i<cardinal_; ++i) {
+    double enb = elements_[i]->getNBoundaries();
+    double ens = elements_[i]->getNSamples();
+    memcpy(boundaries_+boffset,
+	   elements_[i]->getChannelBoundaries(),
+	   enb*sizeof(double));
+    memcpy(widths_+offset,
+	   elements_[i]->getWidths(),
+	   ens*sizeof(double));
+    memcpy(midpoints_+offset,
+	   elements_[i]->getMidpoints(),
+	   ens*sizeof(double));
+    memcpy(chanind_+2*offset,
+	   elements_[i]->getChannelIndices(),
+	   2*ens*sizeof(size_t));
+    boffset += enb;
+    offset  += ens ;
+  }
+  tellListeners();
+}
