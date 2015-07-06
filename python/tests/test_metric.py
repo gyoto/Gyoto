65de725cf92b249483a5b298a9f238ea38a344ce
Thibaut Paumard <paumard@users.sourceforge.net>
2015-07-06 14:56:26 +0200

    Start unit testing of Metric un Python and fix Python example

---
+++ python/tests/test_metric.py
@@ -0,0 +1,17 @@
+import unittest
+import gyoto
+import numpy
+
+gyoto.loadPlugin('stdplug')
+
+class TestMetric(unittest.TestCase):
+
+    def test_circularVelocity(self):
+        gg=gyoto.Metric('KerrBL')
+        vel=numpy.zeros(4, float)
+        vel2=numpy.zeros(4, float)
+        pos_list=(0, 6, numpy.pi*0.5, 0)
+        pos_numpy=numpy.asarray(pos_list)
+        gg.circularVelocity(pos_numpy, vel)
+        gg.circularVelocity(pos_list, vel2)
+        self.assertTrue((vel == vel2).all())
