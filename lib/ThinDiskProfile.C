cdc7837c6d5c62f8073ea2bbfd2e317bc16b7909
Frederic Vincent <frederic.vincent@obspm.fr>
2021-02-02 15:15:57 +0100

    ThinDiskProfile: comment in formula

--- lib/ThinDiskProfile.C
+++ lib/ThinDiskProfile.C
@@ -109,8 +109,9 @@ void ThinDiskProfile::getVelocity(double const pos[4], double vel[4])
       gamma_ms = sqrt(1.-2./(3.*risco)),
       delta = rr*rr - 2.*rr + SPIN*SPIN,
       hh = (2.*rr - SPIN*lambda_ms)/delta;
-    vel[0] = gamma_ms*(1.+2./rr*(1.+hh));
-    vel[1] = -sqrt(2./(3.*risco))*pow(risco/rr-1.,1.5);
+
+    vel[0] = gamma_ms*(1.+2./rr*(1.+hh)); // this is -Ems*g^{tt} + Lms*g^{tp}
+    vel[1] = -sqrt(2./(3.*risco))*pow(risco/rr-1.,1.5); // this is -sqrt{(-1 - g_{tt}*u^t - g_{pp}*u^p - 2*g_{tp}*u^t*u^p)/grr}
     vel[2] = 0.;
     vel[3] = gamma_ms/(rr*rr)*(lambda_ms+SPIN*hh);
 
