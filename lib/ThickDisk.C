1273a716a8e8a6f76a53e3b6b142080676d63bc9
Frederic Vincent <frederic.vincent@obspm.fr>
2021-04-21 07:56:34 +0200

    ThickDisk: big update 	   - matter is everywhere 	   - adding vertical Gaussian dependence of density 	   - velocity is a combination of circular and radial profiles

--- lib/ThickDisk.C
+++ lib/ThickDisk.C
@@ -178,7 +178,12 @@ void ThickDisk::radiativeQ(double Inu[], // output
 
   double number_density = numberDensityAtInnerRadius_cgs_
     *(thickDiskInnerRadius_*thickDiskInnerRadius_)/(rr*rr);
+  double zsigma = rcyl;
 
+  double  expofact_zscaling = exp(-zz*zz/(2.*zsigma*zsigma)); // simple Gaussian modulation around z=0; RIAF model (Broderick+11) use zsigma=rcyl
+  number_density *= expofact_zscaling;
+  //cout << "z/r fact, dens= " << expofact_zscaling << " " << number_density << endl;
+  //cout << "ThickDisk r, z, rho= " << rcyl << " " << zz << " " << number_density << endl;
   double temperature = temperatureAtInnerRadius_
     *pow(thickDiskInnerRadius_/rr, temperatureSlope_);
 
@@ -205,6 +210,10 @@ void ThickDisk::radiativeQ(double Inu[], // output
 		   *GYOTO_PROTON_MASS_CGS * GYOTO_C_CGS * GYOTO_C_CGS
 		   *number_density);
 
+  //cout << "at r z= " << rcyl << " " << zz << endl;
+  //cout << "B= " << BB << endl;
+  //cout << "beta= " << 8.*M_PI*number_density*GYOTO_BOLTZMANN_CGS*temperature/(BB*BB) << endl;
+
   double B0 = 100.; // for ne_inner=5.5e4, B_inner=10.2G; B0=50 too small, B0=100 looks good 
   double DeltaB = B0*gauss2d;
 
@@ -218,7 +227,6 @@ void ThickDisk::radiativeQ(double Inu[], // output
   // double randnb = distribution(generator); // draw a random number
 
   
-  //cout << "r ne B= " << coord_ph[1] << " " << number_density << " " << BB << endl;
   //cout << "IN DISK r, z, ne, B= " << coord_ph[1] << " " << zz << " " << number_density << " " << BB << endl;
   //GYOTO_ERROR("testjet");
 
@@ -226,9 +234,6 @@ void ThickDisk::radiativeQ(double Inu[], // output
     /(2.*M_PI*GYOTO_ELECTRON_MASS_CGS*GYOTO_C_CGS); // cyclotron freq
 
   //cout << "jet stuff= " << coord_ph[1] << " " << coord_ph[2] << " " << zz << " " << rcyljetbase << " " << rcyl << " " << number_density << " " << thetae << " " << temperatureSlope_ << " " << nu0 << endl;
-  //cout << "jet zz,rcyl,th,ph,ne,Te= " <<  zz << " " << rcyl << " " << coord_ph[2] << " " << coord_ph[3] << " " << number_density << " " << temperature << endl;
-  // Use that line for Compton study:
-  //cout <<  zz << " " << rcyl << " " << number_density << " " << temperature << endl;
 
   // Emission and absorption synchrotron coefs
   double jnu_synch[nbnu], anu_synch[nbnu];
@@ -241,15 +246,25 @@ void ThickDisk::radiativeQ(double Inu[], // output
   // THERMAL SYNCHROTRON
   spectrumThermalSynch_->temperature(temperature);
   spectrumThermalSynch_->numberdensityCGS(number_density);
-  spectrumThermalSynch_->angle_averaged(1); // impose angle-averaging
+  spectrumThermalSynch_->angle_averaged(1.); // impose angle-averaging
   spectrumThermalSynch_->angle_B_pem(0.);   // so we don't care about angle
   spectrumThermalSynch_->cyclotron_freq(nu0);
   double besselK2 = bessk(2, 1./thetae);
   spectrumThermalSynch_->besselK2(besselK2);
   //cout << "for anu jnu: " << coord_ph[1] << " " << zz << " " << temperature << " " << number_density << " " << nu0 << " " << thetae << " " << besselK2 << endl;
   //cout << "nu passed to synchro= " << nu_ems[0] << endl;
-  spectrumThermalSynch_->radiativeQ(jnu_synch,anu_synch,
-				    nu_ems,nbnu);
+
+  if (number_density==0.) {
+    // Can happen due to strongly-killing z-expo factor
+    // if zsigma is small. Then leads to 0/0 in synchro stuff. TBC
+    for (size_t ii=0; ii<nbnu; ++ii){
+      jnu_synch[ii]=0.;
+      anu_synch[ii]=0.;
+    }
+  }else{
+    spectrumThermalSynch_->radiativeQ(jnu_synch,anu_synch,
+				      nu_ems,nbnu);
+  }
 
   // RETURNING TOTAL INTENSITY AND TRANSMISSION
   for (size_t ii=0; ii<nbnu; ++ii){
@@ -259,22 +274,14 @@ void ThickDisk::radiativeQ(double Inu[], // output
 
     //cout << "in disk stuff: " << zz << " " << rcyl << " " << nu_ems[0]  << " " << number_density << " " << nu0 << " " << temperature << " " << thetae << " " << jnu_tot << " " << anu_tot << " " << dsem << endl;
 
-    //cout << "in jet stuff: " << number_density << " " << nu0 << " " << thetae << " " << hypergeom << " " << jnu_tot << " " << anu_tot << " " << dsem << endl;
-
-    //cout << "at r,th= " << coord_ph[1] << " " << coord_ph[2] << endl;
-    //cout << "at rcyl,z= " << rcyl << " " << zz << endl;
-    //cout << "jet jnu anu kappa= " << jnu_tot << " " << anu_tot << endl; //x" " << jnu_tot/anu_tot << " " << dsem << endl;
-
     // expm1 is a precise implementation of exp(x)-1
-
-    // TEST compare to Nalewajko
-    //anu_tot=0.;
-    //jnu_tot=1./sqrt(nu_ems[0]);
-    
     double em1=std::expm1(-anu_tot * dsem * gg_->unitLength());
     Taunu[ii] = em1+1.;
     Inu[ii] = anu_tot == 0. ? jnu_tot * dsem * gg_->unitLength() :
       -jnu_tot / anu_tot * em1;
+    //Taunu[ii] = 1.;
+    //Inu[ii] = jnu_tot * dsem * gg_->unitLength() ;
+    //cout << "unit length= " << gg_->unitLength() << " " << jnu_tot * gg_->unitLength() <<  endl;
 
     if (Inu[ii]<0.)
       GYOTO_ERROR("In ThickDisk::radiativeQ: Inu<0");
@@ -303,119 +310,92 @@ double ThickDisk::operator()(double const coord[4]) {
   default:
     GYOTO_ERROR("ThickDisk::operator(): unknown COORDKIND");
   }
-  
-  //if (rproj < thickDiskInnerRadius_)
-  //  return 1.; // outside disk
-  // I think its ok for rproj < rin also TBC
-  
-  double zdisk = 0.;
-  if (rproj > thickDiskInnerRadius_)
-    zdisk = (rproj - thickDiskInnerRadius_)
-      * tan(M_PI/2. - thickDiskOpeningAngle_) ; // altitude of disk at rproj
-  // zdisk is fixed at zero rproj <= rinner,
-  // then the distance to the disk is always positive
 
-  //  cout << "in disk distance, zpos, zdisk= " << zpos << " " << zdisk << endl;
-  
-  return zpos - zdisk; // >0 outside, <0 inside flared disk
+  // // 2019 paper version:
+  // double zdisk = 0.;   // zdisk is fixed at zero rproj <= rinner,
+  // // then the distance to the disk is always positive
+  // double rproj_lim=thickDiskInnerRadius_;
+  // if (rproj > thickDiskInnerRadius_){
+  //   if (rproj > rproj_lim) // usual linear surface above rproj_lim
+  //     zdisk = (rproj - thickDiskInnerRadius_)
+  // 	* tan(M_PI/2. - thickDiskOpeningAngle_) ; 
+  //   else // parabola surface below, connecting continuously
+  //     zdisk = (rproj - thickDiskInnerRadius_)
+  // 	*(rproj - thickDiskInnerRadius_)/(rproj_lim - thickDiskInnerRadius_)
+  // 	* tan(M_PI/2. - thickDiskOpeningAngle_) ;
+  // }  
+  // return zpos - zdisk; // >0 outside, <0 inside flared disk
+
+  // 2021 version without surface
+  return -1.; // matter is everywhere
+
 }
 
 void ThickDisk::getVelocity(double const pos[4], double vel[4])
 {
-  
-  double risco = 0.;
-  if (gg_->kind()!="Minkowski" && gg_->kind()!="Hayward")
-    risco=gg_->getRms(); // prograde Kerr ISCO
-  // let risco=0 if metric is Minko; then ISCO not defined
-  // let also risco=0 for Hayward as we would need to
-  // compute it numerically and give it in xml Metric field,
-  // not implemented so far
-  
-  //cout << "risco= " << risco << endl;
+  double rcyl = pos[1]*sin(pos[2]);// cylindrical radius of current location
 
-  // First, project the current location onto the eqplane
-  double sinth = sin(pos[2]);
-  double coord[4] = {pos[0], pos[1]*sinth, M_PI*0.5, pos[3]};
-  double rcyl = coord[1]; // cylindrical radius of current location
+  double vel_circ[4], vel_rad[4];
+
+  double rr = pos[1];
+  double gtt = gg_->gmunu(pos,0,0),
+    grr = gg_->gmunu(pos,1,1),
+    gpp = gg_->gmunu(pos,3,3),
+    gtp = gg_->gmunu(pos,0,3),
+    guptt = gg_->gmunu_up(pos,0,0),
+    guptp = gg_->gmunu_up(pos,0,3),
+    guppp = gg_->gmunu_up(pos,3,3),
+    guprr = gg_->gmunu_up(pos,1,1);
   
-  // Important Remark: the profile below, both above and below ISCO,
-  // is such that u \cdot u is *not* equal to -1 if one uses the gmunu of the
-  // actual point of emission ("pos", here). The 4vel is normalized when using
-  // the gmunu of the projected point onto the eqplane ("coord", here).
-  // This is Falcke+00 treatment, and RIAF people treatment, as much as I
-  // understand.
-  // Finding a prescription satisfying u \cdot u = -1 with the gmunu
-  // of the point of emission is not trivial, see FV notes BelowISCO.pdf.
+  // CIRCULAR VELOCITY
   
-  if (rcyl > risco){
-    gg_ -> circularVelocity(coord, vel, 1); // Keplerian 4vel at projected point onto eqplane, normalized with gmunu(coord), *not* with gmunu(pos)
-    
-    //gg_ -> circularVelocity(pos, vel, 1); // --> this call would lead to a
-    // properly normalized 4vel, with the gmunu(pos). But then the 4vel
-    // would be discontinuous at risco if one just copy-pastes C75 prescription
-    // below ISCO.
-  }else{
-    // Impose KerrBL here, not clear yet for other metrics
-    string kin = gg_->kind();
-    if (kin != "KerrBL")
-      GYOTO_ERROR("ThickDisk: KerrBL needed below ISCO!");
-    double SPIN = static_cast<SmartPointer<Metric::KerrBL> >(gg_) -> spin();
+  // u_\mu = (u_t,0,0,u_phi) = -u_t (-1,0,0,ll)
+  // with ll = rcyl^{3/2}/(rcyl+1.)
+  
+  double mycst=1; // Gold+20 choice, Keplerian is alpha=-2, 
+  // see Appendix of 2021 M87 paper.
+  double ll=pow(rcyl,1.5)/(rcyl+mycst);
+  double u_t_minus=sqrt(-1./(guptt - 2.*guptp*ll + guppp*ll*ll));
+  double u_t = -u_t_minus, u_phi = u_t_minus*ll;
+  
+  vel_circ[0] = guptt*u_t + guptp*u_phi;
+  vel_circ[1] = 0.;
+  vel_circ[2] = 0.;
+  vel_circ[3] = guptp*u_t + guppp*u_phi;
+  
+  double Omega_circ = vel_circ[3]/vel_circ[0];
+  
+  // RADIAL VELOCITY
+  
+  // 4-vel obtained by imposing: u_t=-1, u_phi=0, u^theta=0
+  // see FV notes SphericalVelocity.pdf for details
+  vel_rad[0] = -guptt;
+  vel_rad[1] = -sqrt((-1.-guptt)*guprr);
+  vel_rad[2] = 0;
+  vel_rad[3] = -guptp;
 
-    // Copy-paste Cunnigham 1975 equatorial profile at all z,
-    // using rcyl for the radial coord
-    double lambda_ms = (risco*risco - 2.*SPIN*sqrt(risco) + SPIN*SPIN)/(pow(risco,1.5) - 2.*sqrt(risco) + SPIN),
-      gamma_ms = sqrt(1.-2./(3.*risco)),
-      delta = rcyl*rcyl - 2.*rcyl + SPIN*SPIN,
-      hh = (2.*rcyl - SPIN*lambda_ms)/delta;
+  double Omega_rad = vel_rad[3]/vel_rad[0];
 
-    vel[0] = gamma_ms*(1.+2./rcyl*(1.+hh)); // this is: -Ems*g^{tt} + Lms*g^{tp}
-    vel[1] = -sqrt(2./(3.*risco))*pow(risco/rcyl-1.,1.5); // this is: -sqrt{(-1 - g_{tt}*u^t - g_{pp}*u^p - 2*g_{tp}*u^t*u^p)/grr}
-    vel[2] = 0.;
-    vel[3] = gamma_ms/(rcyl*rcyl)*(lambda_ms+SPIN*hh);
+  // MIXED VELOCITY
 
-    // Some earlier unconverged tests:
-    
-    // // metric coef at the actual point of emission
-    // double guptt = gg_->gmunu_up(pos,0,0), guptp = gg_->gmunu_up(pos,0,3),
-    //   guppp = gg_->gmunu_up(pos,3,3), gtt = gg_->gmunu(pos,0,0),
-    //   gtp = gg_->gmunu(pos,0,3), gpp = gg_->gmunu(pos,3,3),
-    //   grr=gg_->gmunu(pos,1,1);
-    // // compute geodesic constants E, L at the point P with same z as
-    // // the point of emission, but with rcyl=risco
-    // double zz = pos[1]*cos(pos[2]);
-    // double theta_P = atan2(risco,zz);
-    // double r_P = sqrt(risco*risco + zz*zz);
-    // double pos_P[4] = {pos[0],r_P,theta_P,pos[3]};
-    // double vel_P[4];
-    // // Compute Keplerian 4vel at point P
-    // gg_ -> circularVelocity(pos_P, vel_P, 1);
-    // // Metric coef at point P
-    // double gtt_P = gg_->gmunu(pos_P,0,0), gtp_P = gg_->gmunu(pos_P,0,3),
-    //   gpp_P = gg_->gmunu(pos_P,3,3);
-    // // E and L at this point P
-    // double Ems = -gtt_P*vel_P[0]-gtp_P*vel_P[3],
-    //   Lms = gtp_P*vel_P[0] + gpp_P*vel_P[3];
-    // // We assume that E keeps the same value Ems for all rcyl<risco
-    // // and that L is linearly interpolated between 0 at horizon
-    // // and Lms ar risco
-    // double rhor = 1. + sqrt(1. - SPIN*SPIN), Lhor = 0.; 
-    // double Linterp = Lhor + (rcyl - rhor)/(risco - rhor)*(Lms-Lhor);
-    // // u^t and u^phi at the emission point assuming same E=Ems and L=Linterp
-    // double ut = -Ems*guptt + Linterp*guptp, up = -Ems*guptp + Linterp*guppp;
-    // vel[0] = ut;
-    // vel[1] = -sqrt((-1.-gtt*ut*ut-gpp*up*up-2.*gtp*ut*up)/grr);
-    // vel[2] = 0.;
-    // vel[3] = up;
-    // double u2 = gg_->ScalarProd(pos,vel,vel);
-    // double tol=1e-5;
-    // if (fabs(u2+1.)>tol) {
-    //   cerr << setprecision(10) << "u2= " << u2 << " " << fabs(u2+1.) << endl;
-    //   throwError("In ThickDisk: 4vel below "
-    // 		 "ISCO not properly normalized!");
-    // }
+  double alpha=1., beta=1.;
+
+  vel[1] = vel_circ[1] + (1-alpha)*(vel_rad[1]-vel_circ[1]);
+  vel[2] = 0.;
+  double Omega = Omega_circ + (1-beta)*(Omega_rad-Omega_circ);
+  vel[0] = sqrt(-(1. + grr*vel[1]*vel[1])/(gtt + 2*Omega*gtp + Omega*Omega*gpp));
+  vel[3] = Omega*vel[0];
+
+  //cout << "at rcyl, th-pi/2= " << rcyl << " " << fabs(pos[2]-M_PI/2.) << " u2 = " << gg_->ScalarProd(pos,vel,vel) << endl;
+  double tol=0.03, normcur=gg_->ScalarProd(pos,vel,vel) ;
+  //cout << "4vel at r z= " << pos[1]*sin(pos[2]) << " " << pos[1]*cos(pos[2]) << " " << vel[0] << " " << vel[1] << " " << vel[2] << " " << vel[3] << " " << normcur << endl; // gg_->ScalarProd(pos,vel,vel)
+  
+  if ((fabs(normcur+1.)>tol) ||
+      (normcur!=normcur) ||
+      (normcur==normcur+1)) {
+    cerr << setprecision(10) << "at rcyl th= " << rcyl << " " << pos[2] << ", u2= " << normcur << endl;
+    throwError("In ThickDisk: 4vel not properly normalized!");
   }
-  
-  //cout << "at rcyl= " << rcyl << " u2 = " << gg_->ScalarProd(pos,vel,vel) << endl;
   
 }
 
