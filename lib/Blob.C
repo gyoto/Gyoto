16379ca0dfa3bc1d6c33c06a606672788d2e6104
Frederic Vincent <frederic.vincent@obspm.fr>
2024-12-17 09:36:35 +0100

    Blob: adding helical conical and cylindrical       - remove the older Helical motion kind       - add HelicalCylindrical and HelicalConical that implement either helical mf lines with conical geometry (this was the previous simple "helical" keyword), or a helical mf lines with cylindrical geometry.       - see FV 2024 notes for details

--- lib/Blob.C
+++ lib/Blob.C
@@ -50,7 +50,7 @@ GYOTO_PROPERTY_VECTOR_DOUBLE(Blob, Init4Coord, init4Coord,
 GYOTO_PROPERTY_VECTOR_DOUBLE(Blob, Init3Velo, init3Velo,
 			     "Initial coordinate 3-velocity of the Blob, eg (dr/dt,dtheta/dt,dphi/dt).")
 GYOTO_PROPERTY_STRING(Blob, BlobMotionType, blobMotionType,
-		      "\"Equatorial\" (default), or \"Helical\".")
+		      "\"Equatorial\" (default), \"HelicalConical\", or \"HelicalCylindrical\".")
 GYOTO_PROPERTY_DOUBLE_UNIT(Blob, NumberDensity, numberDensity,
 			   "cgs number density, constant through blob")
 GYOTO_PROPERTY_DOUBLE(Blob, Temperature, temperature,
@@ -167,6 +167,10 @@ void Blob::init4Coord(std::vector<double> const &v) {
   for (size_t i=0; i<n; ++i) {
     init4Coord_[i]=v[i];
   }
+  if (blobMotionType_=="HelicalCylindrical" or blobMotionType_=="Equatorial"){
+    if (init4Coord_[2] != M_PI/2.)
+      GYOTO_ERROR("Equatorial and cylindrical motions must start at equator");
+  }
 }
 std::vector<double> Blob::init4Coord() const {
   std::vector<double> v(4, 0.);
@@ -191,8 +195,10 @@ std::vector<double> Blob::init3Velo() const {
 void Blob::blobMotionType(const string &kind) {
   if(kind == "Equatorial")
     blobMotionType_ = "Equatorial";
-  else if (kind == "Helical")
-    blobMotionType_ = "Helical";
+  else if (kind == "HelicalConical")
+    blobMotionType_ = "HelicalConical";
+  else if (kind == "HelicalCylindrical")
+    blobMotionType_ = "HelicalCylindrical";
   else
     throwError("unknown blob motion type!");
 }
@@ -539,6 +545,10 @@ void Blob::radiativeQ(double *Inu, double *Qnu, double *Unu,
            double const *co) const {
 
   // polarized radiativeQ
+
+  //cout << "In radQ pos vel= " << co[0] << " " << co[1] << " " << co[2] << " " << co[3] << " -- " << co[5]/co[4] << " " << co[7]/co[4] << endl;
+  //cout << "ds in Blob= " << dsem << endl;
+  //cout << "in blob photon tgt in orthon frame= " << coord_ph[4] << " " << coord_ph[5] << " " << coord_ph[1]*coord_ph[6] << " " <<  coord_ph[1]*abs(sin(coord_ph[2]))*coord_ph[7] << endl;
   
   double rr, rcyl, theta, phi, xx, yy, zz=0.;
   switch (gg_->coordKind()) {
@@ -565,6 +575,9 @@ void Blob::radiativeQ(double *Inu, double *Qnu, double *Unu,
     GYOTO_ERROR("In Blob::radiativeQ(): Unknown coordinate system kind");
   }
 
+  //cout << "Blob rcyl= " << rcyl << endl;
+  //cout << "in Blob z= " << blobMotionType_ << " " << zz << endl;
+
   double vel[4]; // 4-velocity of emitter (needed for mf definition below)
   for (int ii=0;ii<4;ii++){
     vel[ii]=co[ii+4];
@@ -585,9 +598,12 @@ void Blob::radiativeQ(double *Inu, double *Qnu, double *Unu,
       ->getCartesian(coord_spot, 1, coord_spot+1, coord_spot+2, coord_spot+3);
     //above: nasty trick to deal with constness of emission
     double xspot=coord_spot[1], yspot=coord_spot[2], zspot=coord_spot[3];
+
+    //cout << "in blob center at t=" << co[0] << " is at xyz=" << xspot << " " << yspot << " " << zspot << endl;
     
     double difx=(xx-xspot), dify=(yy-yspot), difz=(zz-zspot);
     double d2 = difx*difx+dify*dify+difz*difz; // square coord distance between photon and blob's center
+    //cout << "In radQ dist to blob center at tcur; rcur zcur= " << sqrt(d2) << " " << rr << " " << zz << endl;
     
     double blobsize=radius_/3.; // NB: radius_ contains the "blob extension" ie the maximum extension over which Photon::hit returns 1 and emission is computed. This is assumed to coincide with the 3sigma extension of the Gaussian from the blob's center. So the blob's radius stricto sensu is radius_/3.
     double ds2=blobsize*blobsize;
@@ -678,7 +694,8 @@ void Blob::radiativeQ(double *Inu, double *Qnu, double *Unu,
   //cout << endl;
 
   double theta_mag = get_theta_mag(B4vect, coord_ph, vel);
-  //cout << "thetaB=" << theta_mag << endl;
+  //cout << "in Blob thetaB=" << theta_mag << endl;
+  //cout << "at tcur= " << coord_ph[0] << "M = " << coord_ph[0]*21.18/60. << "min" << endl;
   //cout << endl;
   
   Eigen::Matrix4d Omat;
@@ -758,6 +775,12 @@ void Blob::radiativeQ(double *Inu, double *Qnu, double *Unu,
 
   // RETURNING TOTAL INTENSITY AND TRANSMISSION
   for (size_t ii=0; ii<nbnu; ++ii) {
+    
+    // if (jInu[ii] > 2.5e-17){
+    //   cout << "In Blob: ne, temperature, BB, theta_mag, Gauss modul: " << number_density << " " << temperature << " " << BB << " " << theta_mag << " " << total_modulation << endl;
+    //   cout << "In Blob nuem jnu= " << 1.36e14/nuem[ii] << " " << jInu[ii]  << endl;
+    // }
+    //cout << "In Blob nuem jnu= " << 1.36e14/nuem[ii] << " " << jInu[ii]  << endl;
     //cout << "In Blob: jInu, jQnu, jUnu, jVnu: " << jInu[ii] << ", " << jQnu[ii] << ", " << jUnu[ii] << ", " << jVnu[ii] << endl;
     //cout << "In Blob: aInu, aQnu, aUnu, aVnu: " << aInu[ii] << ", " << aQnu[ii] << ", " << aUnu[ii] << ", " << aVnu[ii] << endl;
     //cout << "In Blob: rQnu, rUnu, rVnu: " << rotQnu[ii] << ", " << rotUnu[ii] << ", " << rotVnu[ii] << endl;
@@ -805,13 +828,21 @@ void Blob::getCartesian(double const * const dates, size_t const n_dates,
   double r, theta, phi; // spherical coordinates
   double vel[4];
   
-  if (blobMotionType_=="Helical") // Helical ejection
+  if (blobMotionType_=="HelicalConical") // Helical conical ejection
     {
       r = init4Coord_[1]+init3Velo_[0]*(tt-init4Coord_[0]); // careful: init4Coord_[1] is r, but init3Velo_[0] is dr/dt
       theta = init4Coord_[2];
-      phi = init4Coord_[3] + init4Coord_[1]*init4Coord_[1]*init3Velo_[2]/init3Velo_[0]*(pow(init4Coord_[1],-1.)-pow(r,-1.)); // result of integrale of vphi over time
-      //cout << "t, r, theta, phi = " << tt << ", " << r << ", " << theta << ", " << phi << endl;
+      phi = init4Coord_[3] + init4Coord_[1]*init4Coord_[1]*init3Velo_[2]/init3Velo_[0]*(pow(init4Coord_[1],-1.)-pow(r,-1.)); // result of integral of vphi over time
+      //cout << "t0, r0, vr= " << init4Coord_[0] << " " << init4Coord_[1] << " " <<  init3Velo_[0] << endl;
+      //cout << "In getCart t, r, theta, phi = " << tt << ", " << r << ", " << theta << ", " << phi << endl;
       
+    }
+  else if (blobMotionType_=="HelicalCylindrical")
+    {
+      double tmp = init3Velo_[1] * (tt-init4Coord_[0]); // vth0/r0*t = dthetadt_0 * t (vth0 = r0 * dthetadt_0)
+      r = init4Coord_[1]/2. * (exp(tmp) + exp(-tmp));
+      theta = 2.*atan(exp(tmp));
+      phi = init4Coord_[3] + init3Velo_[2]*(tt-init4Coord_[0]);
     }
   else if (blobMotionType_=="Equatorial") // Equatorial orbit
     {
@@ -851,16 +882,26 @@ void Blob::getVelocity(double const pos[4], double vel[4]){
     vel[1] = 0.;
     vel[2] = 0.;
     vel[3] = init3Velo_[2]; // dphi/dt here, will be multiplied by dt/dtau in normalizeFourVel below
-  }else if (blobMotionType_=="Helical"){
+  }else if (blobMotionType_=="HelicalConical"){
     vel[1] = init3Velo_[0];
     vel[2] = 0.;
     double r0 = init4Coord_[1], rr = pos[1];
     //cout << "in blob velo t, r0, r= " << pos[0] << " " << r0 << " " << rr << endl;
     vel[3] = init3Velo_[2] * r0*r0 / (rr*rr); // assuming conservation of Newtonian ang mom
+    //vel[3] = init3Velo_[2]; // TEST!!!
+  }else if (blobMotionType_=="HelicalCylindrical"){
+    // See FV 2024 notes for details of eqs
+    double tmp = init3Velo_[1] * (pos[0] - init4Coord_[0]); // vth0/r0*t = dthetadt_0 * t
+    vel[1] = init4Coord_[1]*init3Velo_[1]/2. * (exp(tmp) - exp(-tmp)); // dr/dt
+    // Careful: init4Coord_[1] is r ; init3Velo_[1] is dtheta/dt
+    vel[2] = init4Coord_[1]/pos[1]*init3Velo_[1]; // vth = r*dth/dt = cst -> dth/dt = r_0/r * dth/dt_0
+    vel[3] = init3Velo_[2]; // vph = r*sinth*Omega = cst and Omega = cst
+    //cout << "In Blob pos, velos, Velo square= " << pos[0] << " " << pos[1] << " " << pos[2] << " " << pos[3] << " " << pos[1]*cos(pos[2]) << " " << vel[1] << " " << pos[1]*vel[2] << " " <<  pos[1]*sin(pos[2])*vel[3] << " " << init4Coord_[1]*sin(init4Coord_[2])*init3Velo_[2] << " " << vel[1]*vel[1] + pos[1]*pos[1]*vel[2]*vel[2] + pos[1]*pos[1]*sin(pos[2])*sin(pos[2])*vel[3]*vel[3] << endl;
   }
 
-  //cout << "Blob 3vel= " << vel[1] << " " << vel[3] << endl;
+  //cout << "At t, r, theta= " << pos[0] << " " << pos[1] << " " << pos[2] << ", blob 3vel= " << vel[1] << " " << vel[3] << endl;
   
   gg_->normalizeFourVel(pos, vel);
+  //cout << "in blob 4u= " << vel[0] << " " << vel[1] << " " << vel[2] << " " << vel[3] << endl;
 
 }
