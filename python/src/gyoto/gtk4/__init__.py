84eada955bf4e16dd74734d299466550fef31bc6
Thibaut Paumard <thibaut.paumard@obspm.fr>
2026-07-18 09:11:10 +0200

    python: don't import Gtk by default
    This gets in the way of matplotlib which defaults to Qt.

--- python/src/gyoto/gtk4/__init__.py
+++ python/src/gyoto/gtk4/__init__.py
@@ -36,9 +36,6 @@ from gi.repository import GLib
 # Set application name for GTK
 GLib.set_prgname("Gyoto")
 
-# Import submodules
-from . import widgets, utils
-
 # Re-export main entry point for convenience
 
 # Interactive-session entry point:
@@ -100,15 +97,53 @@ def gyotoy(particle=None, blocking=True):
     from .apps.gyotoy import GyotoyApplicationWindow
     return GyotoyApplicationWindow.run(particle, blocking)
 
-# Import for monkey-patching
-from .apps.gyoto_object_editor import GyotoObjectEditor
-
 # Add edit() method to gyoto.core.Object for convenient property editing
 # Note: This should ideally be achieved using SWIG's extend mechanism
 def edit(self, blocking=True):
+    """A GTK4 window for editing Gyoto object properties.
+
+    This window provides a scrollable view of all editable properties of a
+    Gyoto object, using a PropertyEditorBox as its main content.
+
+    The window can run in blocking mode (manages its own GLib main loop) or
+    non-blocking mode (for integration with external event loops like
+    IPython).
+
+    Parameters:
+        obj: The Gyoto object to edit
+        blocking (bool): If True, the window manages the GLib main
+            loop.  If False, the caller must manage the event loop
+            (e.g., using %gui gtk4 in IPython).
+
+    Attributes:
+        obj: The Gyoto object being edited
+        main_loop: GLib.MainLoop instance (if blocking=True)
+        scrolled_window: Gtk.ScrolledWindow containing the editor
+        vbox: PropertyEditorBox for editing object properties
+
+    """
+    from .apps.gyoto_object_editor import GyotoObjectEditor
     GyotoObjectEditor.run(self, blocking=blocking)
-edit.__doc__ = GyotoObjectEditor.run.__doc__
 
 # Monkey-patch the edit method onto gyoto.core.Object
 from ..core import Object
 Object.edit = edit
+
+# Autoload submodules on demand
+def __getattr__(name):
+    """Autoload submodules.
+
+    To avoid issues with loading Gtk when not needed, they should
+    not be loaded by default. This will load them on demand.
+
+    """
+
+    # Avoid infinite recursion for standard attributes
+    if name not in __all__:
+        raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+    fullname = f"{__name__}.{name}"
+
+    mod = importlib.import_module(fullname)
+    setattr(sys.modules[__name__], name, mod)
+    return mod
