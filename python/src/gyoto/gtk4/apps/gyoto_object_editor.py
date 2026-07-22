98adfca586054cbd0c0fdb4f93e4a166e179ac3d
Thibaut Paumard <thibaut.paumard@obspm.fr>
2026-07-22 12:14:05 +0200

    gyoto_object_editor: replace New menu with searchable list

--- python/src/gyoto/gtk4/apps/gyoto_object_editor.py
+++ python/src/gyoto/gtk4/apps/gyoto_object_editor.py
@@ -15,7 +15,7 @@ __all__ = ['GyotoObjectEditorApplication',
 
 import gi
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk, GLib, Gio, Gdk
+from gi.repository import Gtk, GLib, Gio, Gdk, GObject, Pango
 
 import argparse
 import sys
@@ -27,6 +27,17 @@ from ..widgets.property_editor_box import PropertyEditorBox
 from ...core import Factory, Scenery, Error as GyotoError, Photon, Screen
 from ... import core
 
+# Custom GObject class to hold the label of a 'New >' menu item
+class NewItem(GObject.Object):
+    __gtype_name__ = 'NewItem'
+
+    label = GObject.Property(type=str)
+
+    def __init__(self, label=None):
+        super().__init__()
+        self.label = label
+
+# Main application class
 class GyotoObjectEditorApplication(Gtk.Application):
     """Standalone GTK application for the Gyoto Object Editor.
 
@@ -147,7 +158,8 @@ class GyotoObjectEditorApplication(Gtk.Application):
             traceback.print_exc()
         return True
 
-class GyotoObjectEditorApplicationWindow(Gtk.Window):
+# Main application window class
+class GyotoObjectEditorApplicationWindow(Gtk.ApplicationWindow):
     """A GTK4 window for editing Gyoto object properties.
 
     This window provides a scrollable view of all editable
@@ -232,9 +244,7 @@ class GyotoObjectEditorApplicationWindow(Gtk.Window):
         """Build the window's header bar with menu button.
 
         Creates a header bar with a hamburger menu containing:
-        - New...
-          - Star
-          - Photon
+        - New... (opens a popover with search and list)
         - Open...
         - Save
         - Save As...
@@ -250,45 +260,20 @@ class GyotoObjectEditorApplicationWindow(Gtk.Window):
         # Create menu model
         menu = Gio.Menu()
 
-        # New submenu
-        new_menu = Gio.Menu()
+        # Add "New" as a menu item that opens the popover
+        new_menu_item = Gio.MenuItem.new(_("New..."), "win.new")
+        menu.append_item(new_menu_item)
 
-        # Dynamically add items
-        dynamic_items = {
-            'core': ["Scenery", "Screen", "Photon"]
-        }
-        for section in 'Astrobj', 'Metric', 'Spectrum', 'Spectrometer':
-            generic = getattr(core, section)
-            kinds = list(generic.registeredPluginsSlashKinds())
-            kinds.sort()
-            dynamic_items[section] = kinds
+        menu.append(_("Open…"), "win.open")
+        menu.append(_("Save"), "win.save")
+        menu.append(_("Save As…"), "win.save-as")
 
-        for section, items in dynamic_items.items():
-            new_menu_section = Gio.Menu()
-            for label in items:
-                # Create a menu item with the label
-                item = Gio.MenuItem.new(label, "win.new")
-                item.set_action_and_target_value(
-                    "win.new", GLib.Variant("s", section + '/' + label)
-                )
-                new_menu_section.append_item(item)
-            new_menu.append_submenu(section, new_menu_section)
-
-        menu_section1 = Gio.Menu()
-        menu_section1.append_submenu(_("New"), new_menu)
         menu_section2 = Gio.Menu()
-        menu_section2.append(_("Open…"), "win.open")
-        menu_section2.append(_("Save"), "win.save")
-        menu_section2.append(_("Save As…"), "win.save-as")
-        menu_section3 = Gio.Menu()
-        menu_section3.append(_("Help"), "win.help")
-        menu_section3.append(_("Close"), "win.close")
-        menu_section3.append(_("Quit"), "win.quit")
+        menu_section2.append(_("Help"), "win.help")
+        menu_section2.append(_("Close"), "win.close")
+        menu_section2.append(_("Quit"), "win.quit")
 
-        # Main menu items
-        menu.append_section(None, menu_section1)
         menu.append_section(None, menu_section2)
-        menu.append_section(None, menu_section3)
 
         # Create menu button
         menu_button = Gtk.MenuButton(
@@ -298,16 +283,17 @@ class GyotoObjectEditorApplicationWindow(Gtk.Window):
         )
         menu_button.add_css_class("flat")
         header.pack_end(menu_button)
+        self.menu_button=menu_button
+
+        # Build the New popover (parented to the menu button)
+        self.build_new_popover()
 
         # Connect actions
         action_group = Gio.SimpleActionGroup()
         self.insert_action_group("win", action_group)
 
-        action_new = Gio.SimpleAction.new("new", GLib.VariantType("s"))
-        action_new.connect("activate", self.on_new_item_activated)
-        action_group.add_action(action_new)
-
         action_group.add_action_entries([
+            ("new", self.on_new_menu_item_activated, None),
             ("open", self.on_open, None),
             ("save", self.on_save, None),
             ("save-as", self.on_save_as, None),
@@ -315,6 +301,249 @@ class GyotoObjectEditorApplicationWindow(Gtk.Window):
             ("close", self.on_close, None),
             ("quit", self.on_quit, None),
         ])
+
+    def build_new_popover(self):
+        """Build the New popover with search and list."""
+        # Prepare all available items
+        all_items = []
+        for kind in ["Photon", "Scenery", "Screen"]:
+            all_items.append(("core", kind, f"core/built-in/{kind}"))
+        for section in 'Astrobj', 'Metric', 'Spectrometer', 'Spectrum':
+            generic = getattr(core, section)
+            kinds = list(generic.registeredPluginsSlashKinds())
+            kinds.sort()
+            for kind in kinds:
+                all_items.append((section, kind, f"{section}/{kind}"))
+
+        # Create a flat list of all items
+        self.all_new_items = [item[2] for item in all_items]
+
+        # Calculate the minimum width based on the longest label
+        min_width = 300  # Default minimum width
+        if self.all_new_items:
+            # Create a Pango layout to measure text width
+            layout = Pango.Layout(self.get_pango_context())
+            for label in self.all_new_items:
+                layout.set_text(label)
+                width, __ = layout.get_pixel_size()
+                if width > min_width:
+                    min_width = width
+            # Add some padding
+            min_width += 50
+
+        # Create the popover
+        self.new_popover = Gtk.Popover()
+        self.new_popover.set_parent(self.menu_button)
+        self.new_popover.set_autohide(True)
+
+        # Main box
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        main_box.set_size_request(min_width, 400)  # Set minimum size for the main box
+        self.new_popover.set_child(main_box)
+
+        # Title
+        title = Gtk.Label(label=_("New File"), halign=Gtk.Align.START)
+        title.add_css_class("h3")
+        main_box.append(title)
+
+        # Search entry
+        self.new_search_entry = Gtk.SearchEntry()
+        self.new_search_entry.set_placeholder_text(_("Search..."))
+        self.new_search_entry.connect("search-changed", self.on_new_search_changed)
+        self.new_search_entry.set_hexpand(True)  # Allow horizontal expansion
+        main_box.append(self.new_search_entry)
+
+        # Create the list model
+        self.new_list_store = Gio.ListStore(item_type=NewItem)
+        for item in self.all_new_items:
+            self.new_list_store.append(NewItem(label=item))
+
+        # Create a custom filter function
+        def filter_func(item):
+            search_text = self.new_search_entry.get_text().lower()
+            if not search_text:
+                return True
+            return search_text in item.label.lower()
+
+        # Create a custom filter
+        self.custom_filter = Gtk.CustomFilter.new(filter_func)
+
+        # Create filter list model
+        self.new_filter_model = Gtk.FilterListModel(
+            model=self.new_list_store,
+            filter=self.custom_filter
+        )
+
+        # Create selection model
+        self.new_selection = Gtk.SingleSelection(model=self.new_filter_model)
+
+        # Create list view
+        factory = Gtk.SignalListItemFactory()
+        factory.connect("setup", self.on_new_list_item_setup)
+        factory.connect("bind", self.on_new_list_item_bind)
+
+        self.new_list_view = Gtk.ListView(
+            model=self.new_selection,
+            factory=factory
+        )
+        self.new_list_view.set_hexpand(True)  # Expand horizontally
+        self.new_list_view.set_vexpand(True)  # Expand vertically
+        self.new_list_view.add_css_class("rich-list")
+        self.new_list_view.connect("activate", self.on_new_list_activate)
+
+        # Add list view to a scrolled window
+        scrolled = Gtk.ScrolledWindow()
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)  # No horizontal scroll, auto vertical
+        scrolled.set_child(self.new_list_view)
+        scrolled.set_hexpand(True)
+        scrolled.set_vexpand(True)
+        scrolled.set_min_content_height(300)  # Minimum height for the list
+        main_box.append(scrolled)
+
+        # Buttons
+        button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        button_box.add_css_class("linked")
+
+        cancel_button = Gtk.Button(label=_("Cancel"))
+        cancel_button.connect("clicked", lambda *_: self.new_popover.popdown())
+        button_box.append(cancel_button)
+
+        create_button = Gtk.Button(label=_("Create"))
+        create_button.add_css_class("suggested-action")
+        create_button.connect("clicked", self.on_new_create_clicked)
+        button_box.append(create_button)
+
+        main_box.append(button_box)
+
+        # Add key controller to the search entry for keyboard navigation
+        key_controller = Gtk.EventControllerKey()
+        key_controller.connect("key-pressed", self.on_new_key_pressed)
+        self.new_search_entry.add_controller(key_controller)
+
+        # Add key controller to the list view for Up/Down navigation
+        list_key_controller = Gtk.EventControllerKey()
+        list_key_controller.connect("key-pressed", self.on_list_key_pressed)
+        self.new_list_view.add_controller(list_key_controller)
+
+    def filter_new_items(self, item, search_entry):
+        """Filter function for the New items list."""
+        search_text = search_entry.get_text().lower()
+        if not search_text:
+            return True
+        return search_text in item.label.lower()
+
+    def on_new_menu_item_activated(self, action, parameter, *args):
+        """Show the New popover when the New menu item is activated."""
+        self.new_popover.popup()
+
+    def on_new_button_clicked(self, button, *args):
+        """Show the New popover."""
+        self.new_popover.popup()
+
+    def on_new_search_changed(self, entry):
+        """Update filter when search text changes."""
+        self.custom_filter.changed(Gtk.FilterChange.DIFFERENT)
+
+    def on_new_list_item_setup(self, factory, list_item):
+        """Set up list items."""
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        list_item.set_child(box)
+
+        # Label for the item
+        label = Gtk.Label(halign=Gtk.Align.START, xalign=0)
+        label.add_css_class("title")
+        box.append(label)
+
+    def on_new_list_item_bind(self, factory, list_item):
+        """Bind list items to data."""
+        item = list_item.get_item()
+        label = list_item.get_child().get_first_child()
+        label.set_text(item.label)
+
+    def on_new_list_activate(self, list_view, position):
+        """Handle activation (Enter key) in the list."""
+        self.on_new_create_clicked()
+
+    def on_new_key_pressed(self, controller, keyval, keycode, state):
+        """Handle keyboard navigation in the search entry."""
+        if keyval == Gdk.KEY_Down:
+            # Move focus to list view and select first item
+            self.new_list_view.grab_focus()
+            if self.new_filter_model.get_n_items() > 0:
+                self.new_selection.set_selected(0)
+            return True
+        return False
+
+    def on_new_create_clicked(self, *args):
+        """Create the selected item."""
+        if self.new_selection.get_selected_item() is None:
+            return
+
+        selected_item = self.new_selection.get_selected_item()
+        label = selected_item.label
+
+        nspace, plg, kind = label.split('/', 2)
+
+        if nspace == 'core':
+            obj = getattr(core, kind)()
+        else:
+            obj = getattr(core, nspace)(kind, (plg,))
+
+        window = GyotoObjectEditorApplicationWindow(
+            application=self.props.application,
+            obj=obj,
+            connector=None
+        )
+        window.present()
+
+        self.new_popover.popdown()
+
+    def on_list_key_pressed(self, controller, keyval, keycode, state):
+        """Handle keyboard navigation in the list view."""
+        if (
+                (hasattr(Gdk, 'KEY_Enter') and keyval == Gdk.KEY_Enter)
+                or (hasattr(Gdk, 'KEY_Return') and keyval == Gdk.KEY_Return)
+        ):
+            self.on_new_create_clicked()
+            return True
+        return False
+
+    ####################################################################
+    # Callbacks
+    ####################################################################
+
+    def on_new_item_activated(self, action, parameter):
+        """Create a new object and open an editor window
+
+        Args:
+            action: the Gio Action
+            parameter: its parameter
+
+        """
+        obj = None
+        label = parameter.get_string()  # Extract the label
+
+        static_constructors = {
+            "core/Scenery": Scenery,
+            "core/Photon": Photon,
+            "core/Screen": Screen,
+        }
+
+        if label in static_constructors:
+            obj = static_constructors[label]()
+        else:
+            nspace, plg, kind = label.split('/', 2)
+            obj = getattr(core, nspace)(kind, (plg,))
+
+        if obj is None:
+            raise ValueError(f'no valid constructor for {label}')
+
+        window = GyotoObjectEditorApplicationWindow(
+            application=self.props.application,
+            obj=obj,
+            connector=None
+        )
+        window.present()
 
     def build_shortcuts(self):
         """Create keyboard shortcuts.
@@ -330,6 +559,16 @@ class GyotoObjectEditorApplicationWindow(Gtk.Window):
         """
         controller = Gtk.ShortcutController()
         self.add_controller(controller)
+
+        controller.add_shortcut(
+            Gtk.Shortcut(
+                trigger=Gtk.KeyvalTrigger(
+                    keyval=Gdk.KEY_n,
+                    modifiers=Gdk.ModifierType.CONTROL_MASK
+                ),
+                action=Gtk.CallbackAction.new(self.on_new_button_clicked)
+            )
+        )
 
         controller.add_shortcut(
             Gtk.Shortcut(
